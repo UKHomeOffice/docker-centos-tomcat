0890578f1b10173adb9ba67b4dc4d2d359ebe3d9
Mark Olliver <mark@keao.cloud>
2016-06-14 16:57:09 +0100

    fix missing then

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-if [ -f "/opt/tomcat/ssl/tomcat-users.xml" ]
+if [ -f "/opt/tomcat/ssl/tomcat-users.xml" ]; then
     echo "Passwords found - running with basic auth"
     cp /opt/tomcat/conf/web.xml.password /opt/tomcat/conf/web.xml
     exec catalina.sh run
